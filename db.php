04a191f35291f1f1a2c67e78f4b0720e289b5cf6
krutikamalgi <112268567+krutikamalgi@users.noreply.github.com>
2022-09-23 16:07:47 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,6 +2,6 @@
 $mysql_hostname = "localhost";
 $mysql_user = "root";
 $mysql_password = "";
-$mysql_database = "profile";
+$mysql_database = "member";
 $con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
 ?>
